fe2952520d7df20ceb5024bf89f6ac477319bd00
Mathew Glodack <glodack@gmail.com>
2016-10-14 21:51:08 -0700

    Initial Project Setup

---
+++ src/FSharp.Bugsnag/Bugsnag.fs
@@ -0,0 +1,21 @@
+﻿namespace FSharp.Bugsnag
+
+module Client =
+  type Severity = Error | Warning | Info
+
+  type ReleaseStage = Development | Staging | Production
+
+  type BugsnagConfig =
+    {
+      ApiKey: string option
+      ReleaseStage: ReleaseStage
+      UseSSL: bool
+    }
+
+  let BugsnagConfigDefaults () =
+    {
+      ApiKey = None
+      ReleaseStage = ReleaseStage.Development
+      UseSSL = true
+    }
+
