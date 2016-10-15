834ad176d22ad253cbe0deb8465c61e6d4a7fd5d
Mathew Glodack <glodack@gmail.com>
2016-10-15 14:32:14 -0700

    Add StackTrace Line Parsing

--- src/FSharp.Bugsnag/ExceptionConverter.fs
+++ src/FSharp.Bugsnag/ExceptionConverter.fs
@@ -1,13 +1,52 @@
 ﻿namespace FSharp.Bugsnag
 
 module ExceptionConverter =
-  open System
   open FSharp.Bugsnag.Types
+  open System
+  open System.Text.RegularExpressions
+
+  [<Literal>]
+  let private _unknownType = "UNKNOWN_TYPE"
+
+  [<Literal>]
+  let private _unknownMethod = "UNKNOWN_METHOD"
 
   let rec unwrapException (ex : System.Exception) =
     match ex with
     | null -> List.empty<System.Exception>
     | _ -> ex :: (unwrapException ex.InnerException)
+
+  let parseFile (line : string) =
+    let ``match`` = Regex.Match(line, "in (.+):line");
+    if (``match``.Groups.Count < 2) then "[file]"
+    else ``match``.Groups.Item(1).Value
+
+  let parseMethodName (line : string) =
+    let ``match`` = Regex.Match(line, "at ([^)]+[)])")
+    if (``match``.Groups.Count < 2) then "[method]"
+    else ``match``.Groups.Item(1).Value
+
+  let parseLineNumber (line : string) =
+    let ``match`` = Regex.Match(line, ":line ([0-9]+)")
+    if (``match``.Groups.Count < 2) then -1
+    else Convert.ToInt32(``match``.Groups.Item(1).Value)
+
+  let convertToStackTrace (line : string) : StackTrace =
+    {
+        File = parseFile line
+        LineNumber = parseLineNumber line
+        ColumnNumber = None
+        Method = parseMethodName line
+        InProject = None
+      }
+
+  let convertStackTrace (stackTrace : string) : StackTrace list =
+    match stackTrace with
+    null -> List.empty<StackTrace>
+    | _ ->
+      stackTrace.Split([|'\n'|], StringSplitOptions.RemoveEmptyEntries)
+      |> Array.toList
+      |> List.map convertToStackTrace
 
   let convertToBugsnagExceptions(unwrappedExceptions : System.Exception list) =
     unwrappedExceptions
@@ -16,12 +55,12 @@ module ExceptionConverter =
         ErrorClass = ex.GetType().Name
         Message = Some(ex.Message)
         // TODO: Fill in stacktrace
-        StackTrace = []
+        StackTrace = convertStackTrace ex.StackTrace
       })
 
   let getTargetSite (ex : System.Exception) =
     match ex.TargetSite with
-    null -> "TODO: Find a better default"
+    null -> sprintf "%s::%s" _unknownType _unknownMethod
     | _ -> ex.TargetSite.Name
 
   let getGroupingHash (ex : System.Exception) =
@@ -31,7 +70,7 @@ module ExceptionConverter =
 
   let getDefaultContext (ex : System.Exception) =
     match ex.TargetSite with
-    | null -> "UNKNOWN_TYPE::UNKNOWN_METHOD"
+    | null -> sprintf "%s::%s" _unknownType _unknownMethod
     | _ -> sprintf "%A::%s" (ex.TargetSite.DeclaringType) (ex.TargetSite.Name)
 
   let convertWithContext (settings : BugsnagAppSettings, severity : Severity, getContext, ex : System.Exception) =
