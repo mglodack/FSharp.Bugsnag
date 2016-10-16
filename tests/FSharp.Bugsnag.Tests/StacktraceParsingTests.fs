3fc6d314c927d03b45dd711b6437441a621d7af3
Mathew Glodack <glodack@gmail.com>
2016-10-16 14:24:31 -0700

    Add FSharp Test Project w/ NuGet Deps
    * Add intiail stacktrace helper tests

---
+++ tests/FSharp.Bugsnag.Tests/StacktraceParsingTests.fs
@@ -0,0 +1,36 @@
+﻿module StacktraceParsingTests
+
+open Xunit
+open FsUnit.Xunit
+open FSharp.Bugsnag.Formatting.StacktraceParser
+
+[<Literal>]
+let basicStacktraceLine = "at Microsoft.FSharp.Core.Operators.Raise[T](Exception exn)"
+
+[<Literal>]
+let explicitStacktraceLine = "at <StartupCode$query_xaxkqc>.$Main.main@() in C:\Users\glodack\AppData\Local\Temp\LINQPad5\_hemovvds\query_xaxkqc.fs:line 6"
+
+[<Fact>]
+let ``parseFile should return [file] when not found``() =
+  parseFile basicStacktraceLine
+  |> should equal "[file]"
+
+[<Fact>]
+let ``parseFile should return correc when found``() =
+  parseFile explicitStacktraceLine
+  |> should equal "C:\Users\glodack\AppData\Local\Temp\LINQPad5\_hemovvds\query_xaxkqc.fs"
+
+[<Fact>]
+let ``parseMethodName should return method name when found``() =
+  parseMethodName basicStacktraceLine
+  |> should equal "Microsoft.FSharp.Core.Operators.Raise[T](Exception exn)"
+
+[<Fact>]
+let ``parseLineNumber should return line number when exists``() =
+  parseLineNumber explicitStacktraceLine
+  |> should equal 6
+
+[<Fact>]
+let ``parseLineNumber should return -1 when line number is not found``() =
+  parseLineNumber basicStacktraceLine
+  |> should equal -1
